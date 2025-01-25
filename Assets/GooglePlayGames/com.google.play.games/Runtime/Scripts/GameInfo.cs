a5d434d2d043d5c349894dcc5c600967a43d4dd9
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2025-01-25 21:50:28 +0900

    amSSSIB
    GPGS dogseKKi

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "877695824920"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "877695824920-m4d1v4bjgp94ooj8sank97qsdh5b4vm5.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
--- Assets/Resource/Script/GPGS/GPGS_LogIn.cs
+++ Assets/Resource/Script/GPGS/GPGS_LogIn.cs
@@ -13,13 +13,36 @@ public class GoogleManager : MonoBehaviour
     {
         PlayGamesPlatform.DebugLogEnabled = true;   //gpgs 전용 디버그창 키기
         PlayGamesPlatform.Activate();   //gpgs 작동 시작
-        SignIn();   //구글계정 연동(로그인)함수 호출
+       
+
+        //기존에 사용하던 계정이 있다면
+        if (PlayerPrefs.HasKey("SavedAccountKey")) 
+        {
+            AutoSignIn();
+        }
+        else
+        {
+            //처음 로그인 시도러면
+
+            ManualSignIn();
+        }
+
     }
 
-    public void SignIn()    //구글계정 로그인 함수
+    public void AutoSignIn()    //구글계정 자동 로그인 함수
     {
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication); //gpgs 싱글톤 클래스의 로그인 함수 호출(변수로 콜백 함수 ProcessAuthentication 등록)
     }
+
+
+    public void ManualSignIn()  //구글 계정 수동 로그인 함수
+    {
+        
+        PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication); //로그인 계정 선택 화면을 표시, 계정 선택 후 콜백 함수 ProcessAuthentication 호출
+    }
+
+
+
 
     internal void ProcessAuthentication(SignInStatus status)
     {
@@ -28,17 +51,22 @@ public class GoogleManager : MonoBehaviour
         {
             //로그인에 성공했다면 작동
 
-            string name = PlayGamesPlatform.Instance.GetUserDisplayName();     //유저명 받아오기
+            
             string id = PlayGamesPlatform.Instance.GetUserId();             //유저 id값 받아오기
-            string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();   //유저 아이콘 받아오기
+            PlayerPrefs.SetString("SavedAccountKey", id);                   //자동 로그인 여부 확인을 위해 id값을 player prefs에 저장
+            PlayerPrefs.Save();
 
-            logText.text = "Success \n" + name; //로그에 "Success"와 유저명 출력
+
+            logText.text = "Success"; //로그에 "Success" 출력
         }
         else
         {
             //로그인에 실패했다면
-            logText.text = "Sign in Failed!";   //로그에 "Sign in Failed!" 출력
+            logText.text = "Sign in Failed! " + status.ToString();   //로그에 "Sign in Failed!" 출력
 
+
+            //다른 게정 사용 유도를 위해 수동 로그인 호출
+            ManualSignIn();
 
             // 그 외 로그인 실패시 처리할 사항 기술
         }
