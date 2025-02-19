65d70eff949a6fdfdcb5e4b7e7a1d70b1a554e2d
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2025-02-19 17:04:42 +0900

    초기 정보 세팅
    전투 씬에서 사용될 스탯 및 기타 정보들의 초기 세팅 기능 구현

--- Assets/Resources/Script/GPGS/tempScriptForDebug.cs
+++ Assets/Resources/Script/GPGS/tempScriptForDebug.cs
@@ -7,24 +7,29 @@ public class tempScriptForDebug : MonoBehaviour
 {
 
     public TextMeshProUGUI goldText;
+    public TextMeshProUGUI HpText;
+    public TextMeshProUGUI ATKText;
+    public TextMeshProUGUI BcText;
+    public TextMeshProUGUI PPText;
 
 
     private void Start()
     {
         goldText.text = "0";
+        HpText.text = "0";
+        ATKText.text = "0";
+        BcText.text = "0";
+        PPText.text = "0"; 
     }
 
     private void Update()
     {
-        if (PlayerPrefs.HasKey("Gold"))
-        {
-            goldText.text = DataControl.LoadEncryptedDataFromPrefs("Gold");
-        }
-        else
-        {
-            DataControl.SaveEncryptedDataToPrefs("Gold", goldText.text);
-            goldText.text = DataControl.LoadEncryptedDataFromPrefs("Gold");
-        }
+        goldText.text = DataControl.LoadEncryptedDataFromPrefs("Gold");
+        HpText.text = DataControl.LoadEncryptedDataFromPrefs("PlayerCharacter_HP");
+        ATKText.text = DataControl.LoadEncryptedDataFromPrefs("PlayerCharacter_ATK");
+        BcText.text = DataControl.LoadEncryptedDataFromPrefs("PlayerCharacter_BALLCOUNT");
+        PPText.text = DataControl.LoadEncryptedDataFromPrefs("PlayerCharacter_PINHP");
+
     }
 
 
@@ -41,6 +46,36 @@ public class tempScriptForDebug : MonoBehaviour
         int tempGold = int.Parse(goldText.text) - 100;
         DataControl.SaveEncryptedDataToPrefs("Gold", tempGold.ToString());
     }
+
+
+
+    public void plusHP()
+    {
+        int temp = int.Parse(HpText.text) + 1;
+        DataControl.SaveEncryptedDataToPrefs("PlayerCharacter_HP", temp.ToString());
+    }
+
+
+    public void plusATK()
+    {
+        int temp = int.Parse(ATKText.text) + 1;
+        DataControl.SaveEncryptedDataToPrefs("PlayerCharacter_ATK", temp.ToString());
+    }
+
+
+    public void plusBC()
+    {
+        int temp = int.Parse(BcText.text) + 1;
+        DataControl.SaveEncryptedDataToPrefs("PlayerCharacter_BALLCOUNT", temp.ToString());
+    }
+
+
+    public void plusPP()
+    {
+        int temp = int.Parse(PPText.text) + 1;
+        DataControl.SaveEncryptedDataToPrefs("PlayerCharacter_PINHP", temp.ToString());
+    }
+
 
 
 }
