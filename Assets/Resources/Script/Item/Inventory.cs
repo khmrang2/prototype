75f146f6774cddad42502007e2f1729c78a63565
khmrang2 <ghdj6625@hanyang.ac.kr>
2025-03-05 01:34:23 +0900

    코드 정리
    1. 800x480에서 갑자기 인벤토리의 슬롯이 사라지는 버그 수정

--- Assets/Resources/Script/Item/Inventory.cs
+++ Assets/Resources/Script/Item/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,49 +35,72 @@ public class Inventory : MonoBehaviour
     public List<Item> items = new List<Item>();
     public List<GameObject> slots = new List<GameObject>();
 
-
+    /// <summary>
+    /// 데이터에서 획득한 아이템을 기반으로 데이터를 로드하고 불러온다.
+    /// </summary>
     private void Start()
     {
+        //획득한 아이템을 기반으로 데이터를 로드하고 불러오는 코드를 작성.
+        //database.dataload();
         //database = GetComponent<ItemDatabase>();
 
         slotAmount = 16;
 
         for(int i = 0; i < slotAmount; i++)
         {
-            // 슬롯만 생성하고, 아이템이 있는지 확인.
-            items.Add(new Item());
+            // 빈 아이템 슬롯을 생성.
             slots.Add(Instantiate(inventorySlot));
+            // 빈 아이템 리스트에 생성.
+            items.Add(new Item());
+            // 생성한 아이템 슬롯을 뷰어 오브젝트(contents)의 자식으로 할당.
             slots[i].transform.SetParent(slotPanel.transform, false);
         }
-        AddItem(1);
-        AddItem(2);
-        AddItem(3);
+        AddItem(1, 0);
+        //AddItem(2);
+        //AddItem(3);
     }
 
-    public void AddItem(int id)
+    /// <summary>
+    /// 아이템을 인벤토리에 추가하는 코드.
+    /// </summary>
+    /// <param name="id"></param>
+    public void AddItem(int id, int amount)
     {
-        // 아이템을 바로 추가. 
+        // 아이템 코드 데이터베이스에서 id를 통해 아이템을 넣는다.
+        // 즉, 아이템 코드로 우리는 아이템들을 불러올 수 있다.
         Item itemToAdd = database.FetchItemById(id);
         for (int i = 0; i < items.Count; i++)
         {
-            // 슬롯에 아이템이 존재하는가?
+            // 슬롯이 비어있다면(해당 슬롯에 아이템이 없는 상태)
             if (items[i].Id == -1)
             { 
-                // 존재하면 id로 불러온 아이템을 넣어준다. 해당 슬롯으로,
+                // 아이템 리스트에 넣고,
                 items[i] = itemToAdd;
+                // 프리팹으로 아이템을 생성.
                 GameObject itemObj = Instantiate(inventoryItem);
-                // 부모의 위치에서
+
+                // 아이템 슬롯을 부모로 하여 생성.
                 itemObj.transform.SetParent(slots[i].transform, false);
-                // 0,0에 위치하도록.
+                // 앵커 오류로 인해서 넣은 오류 수정 코드(상동한 코드다.)
                 RectTransform rect = itemObj.GetComponent<RectTransform>();
                 rect.anchoredPosition = Vector2.zero;
                 // 이미지 가져오고.
                 itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
-                // 툴팁으로 이미지 이름 설정.
+                // 툴팁으로 오브젝트의 이름을 설정.
                 itemObj.name = itemToAdd.Tooltip;
+                // 그다음 오브젝트에서 양도 가져온다.
+                itemObj.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "12";
                 break;
             }
         }
+    }
+
+    /// <summary>
+    /// 인벤토리를 레어도 순으로 정렬하는 코드.
+    /// </summary>
+    public void sortInventory()
+    {
+        return;
     }
 
 
