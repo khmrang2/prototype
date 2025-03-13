567ec3cea4450f3e26e8b01ecf8f07b161de6f43
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2025-03-13 20:32:25 +0900

    rr
    rr

--- Assets/Resources/Script/Enemy.cs
+++ Assets/Resources/Script/Enemy.cs
@@ -5,10 +5,12 @@ public class Enemy : MonoBehaviour
 {
     private GameObject target;  // 이동할 목표 오브젝트
     public float moveSpeed = 5f;  // 이동 속도
-    private bool isMoving = false; // 이동 여부 확인
+    [SerializeField] private bool isMoving = false; // 이동 여부 확인
     public bool isDetectedPlayer = false;   //플레이어 감지 여부
     private PlayerManger Pmanager;  //플레이어에게 데미지 처리를 위한 PlayerManager
     private EnemyStatus status; //이 적 케릭터의 스탯
+    public bool isSpawned = false;
+    public bool isAlive;
     //private Animator animator; // 애니메이터(공격 에니메이션 추가 시 사용될 예정)
 
     [SerializeField] private float AttackRange = 0.1f;
@@ -35,23 +37,38 @@ public class Enemy : MonoBehaviour
         //플레이어 감지 상태를 false로 초기화
         isDetectedPlayer = false;
 
+        //스폰 처리를 false로 초기화
+        //isSpawned = false;
+
+        //생존 처리를 true로
+        isAlive = true;
         //추후 공격 에니메이션 추가 시 사용될 용도
         //animator = GetComponent<Animator>();
     }
 
     public async Task Move()
     {
-        if (target != null && !isMoving) // 타겟이 존재하고 이동 중이 아닐 때만 이동 시작
+        if (target != null && !isMoving) // 타겟이 존재하고 소환이 완료되었으며 이동 중이 아닐 때만 이동 시작
         {
-            //사거리 내에 플레이어가 존재하면
-            if (isDetectedPlayer)
+            //스폰 처리가 되었을 때만 이동 여부 확인
+            if (isSpawned)
             {
-                await Attack(); //공격이 끝날 때까지 대기
-                isMoving = false;   //이동이 불필요하므로 false
+                //사거리 내에 플레이어가 존재하면
+                if (isDetectedPlayer)
+                {
+                    await Attack(); //공격이 끝날 때까지 대기
+                    isMoving = false;   //이동이 불필요하므로 false
+                }
+                else
+                {
+                    isMoving = true;    //사거리 내에 적이 들어와야 하니 이동
+                }
+
             }
-            else 
+            else
             {
-                isMoving = true;    //사거리 내에 적이 들어와야 하니 이동
+                //스폰 처리가 안되었다면 이동하면 안되니 false
+                isMoving = false ;
             }
         }
     }
@@ -75,7 +92,8 @@ public class Enemy : MonoBehaviour
 
     public bool HasMoved()
     {
-        return isMoving;
+        //Debug.Log(isMoving);
+        return !isMoving;
     }
 
     public void ResetMove()
