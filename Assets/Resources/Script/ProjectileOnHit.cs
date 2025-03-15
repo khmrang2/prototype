d7ff04b1b388ba081f4294d09813297c429d5532
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2025-03-15 22:07:59 +0900

    데미지 관련 개발
    1. 적 케릭터 데미지 받는 기능
    2. 적과 플레이어 체력 0 되면 죽는 기능

--- Assets/Resources/Script/ProjectileOnHit.cs
+++ Assets/Resources/Script/ProjectileOnHit.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental;
 using UnityEngine;
 
 public class ProjectileOnHit : MonoBehaviour
 {
+
+    private int atk;
+    public int hitCount;
+
+    private void Awake()
+    {
+        PlayerStatus ps = FindObjectOfType<PlayerStatus>();
+
+        if (ps != null)
+        {
+            atk = ps.GetTotalDamage();
+            Debug.Log("-----------Total Damage: " +  atk);
+        }
+        else { Debug.LogError("플레이어 스탯 참조 실패!"); }
+    }
+
     //public string targetTag;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy")) 
         {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+            enemy.status.EnemyHP -= atk;
+
             Destroy(this.gameObject);
         }
 
