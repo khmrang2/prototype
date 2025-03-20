d9e5f669f1be7d23339efaaa262c7d63a3f1d6c5
khmrang2 <ghdj6625@hanyang.ac.kr>
2025-03-20 09:15:14 +0900

    UIMerge
    .

--- Assets/Resources/Script/UI/UpgradeScrollwithContents.cs
+++ Assets/Resources/Script/UI/UpgradeScrollwithContents.cs
@@ -19,51 +19,12 @@ public class UpgradeScrollwithContents : MonoBehaviour
     void Start()
     {
         scrollRect.onValueChanged.AddListener(_ => OnScroll());
-
-        // ��� ������Ʈ Ǯ ����
-        for (int i = 0; i < poolSize; i++)
-        {
-            var bg = Instantiate(bgPrefab, bgParent);
-            bg.anchoredPosition = new Vector2(0, i * bgHeight);
-            bg.gameObject.SetActive(true);  // ó������ ��� Ȱ��ȭ
-            bgPool.Add(bg);
-        }
     }
 
     void OnScroll()
     {
         float contentY = contents.anchoredPosition.y;
 
-        // ��� ��ġ ����
-        foreach (var bg in bgPool)
-        {
-            float bgY = bg.anchoredPosition.y;
-            float distance = Mathf.Abs(contentY - bgY);
-
-            // ������ �þ߿��� ��� ����� ��Ȱ��ȭ
-            bool isVisible = distance < bgHeight + visibilityOffset;
-            bg.gameObject.SetActive(isVisible);
-        }
-
-        // ��ũ�� ���⿡ ���� ��� ����
-        if (contentY > lastContentY) // �Ʒ��� ��ũ��
-        {
-            RecycleBackground();
-        }
         lastContentY = contentY;
-    }
-
-    void RecycleBackground()
-    {
-        RectTransform firstBg = bgPool[0];
-        RectTransform lastBg = bgPool[^1];
-
-        // ù ��° ����� ȭ�� ���� ����ٸ� ����
-        if (firstBg.anchoredPosition.y + bgHeight < contents.anchoredPosition.y - visibilityOffset)
-        {
-            firstBg.anchoredPosition = new Vector2(0, lastBg.anchoredPosition.y + bgHeight);
-            bgPool.RemoveAt(0);
-            bgPool.Add(firstBg);
-        }
     }
 }
