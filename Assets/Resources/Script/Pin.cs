ff56d0ef7bfc25234a63615c2f6d6193c767a6ae
ryusiwoo0112 <105357700+ryusiwoo0112@users.noreply.github.com>
2025-03-21 18:37:34 +0900

    BugFix

--- Assets/Resources/Script/Pin.cs
+++ Assets/Resources/Script/Pin.cs
@@ -16,7 +16,7 @@ public class Pin : MonoBehaviour
     private float forceOffset = 15.0f;
 
     private int cnt = 0;
-    private int maxHitCount = 5; // 최대 충돌 횟수
+    private int maxHitCount = int.Parse(DataControl.LoadEncryptedDataFromPrefs("PlayerCharacter_PINHP")); // 최대 충돌 횟수
 
     [Header("Player Stat Script")]
     public PlayerState playerState;
