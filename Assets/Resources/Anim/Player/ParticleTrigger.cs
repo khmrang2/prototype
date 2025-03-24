0e70745d2185209c1e4e5f8bd29189895edf3ef0
khmrang2 <ghdj6625@hanyang.ac.kr>
2025-03-24 22:37:35 +0900

    적용 전 애니메이션 테스트
    .

---
+++ Assets/Resources/Anim/Player/ParticleTrigger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ParticleTrigger : MonoBehaviour
+{
+    public ParticleSystem[] impactParticles; // 여러 파티클 시스템
+
+    public void PlayImpactParticle(int index)
+    {
+        if (impactParticles != null && impactParticles.Length > index)
+        {
+            var selectedParticle = impactParticles[index];
+            selectedParticle.Play();  // 선택한 파티클 실행
+        }
+    }
+}
