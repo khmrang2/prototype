71667e9ed19c619f195b2f7856dce1837226c639
20yeon <127908102+20yeon@users.noreply.github.com>
2025-04-01 18:17:23 +0900

    버그 픽스
    사운드바 앵커설정, 소리 설정 저장, 광고 한번 보면 다음날 다시 볼수있게 조정

--- Assets/Resources/Script/UI/BackManager.cs
+++ Assets/Resources/Script/UI/BackManager.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio; // 이게 꼭 있어야 AudioMixer를 씁니다
 
 public class BackManager : MonoBehaviour
 {
     public static BackManager Instance { get; private set; }
 
     public event Action OnBackButtonPressed;
+    [SerializeField] private AudioMixer audioMixer;
 
     private void Awake()
     {
@@ -16,6 +18,8 @@ public class BackManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+        if(DataControl.LoadEncryptedDataFromPrefs("bgm_volume") != null) audioMixer.SetFloat("BGM", float.Parse(DataControl.LoadEncryptedDataFromPrefs("bgm_volume")));
+        if(DataControl.LoadEncryptedDataFromPrefs("effect_volume") != null) audioMixer.SetFloat("Effect", float.Parse(DataControl.LoadEncryptedDataFromPrefs("bgm_volume")));
     }
 
     private void Update()
